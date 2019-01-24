4c44bf3bfe6547c9b375930ebb5ecda558a0a0c5
Li penghui <lipenghui@caxins.com>
2019-01-24 18:19:32 +0800

    基本架构

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,10 +1,4 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h1>跳转至HelloVue</h1>
-    <h1>
- <router-link to="day01">跳转至HelloVue</router-link>
-</h1>
  <el-container>
   <el-header>Header</el-header>
   <el-container>
@@ -15,9 +9,7 @@
     </el-container>
   </el-container>
 </el-container>
-  </div>
 </template>
-
 <script>
 export default {
   name: 'HelloWorld',
--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
+import VueRouter from 'vue-router'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+Vue.use(VueRouter)
 
 /* eslint-disable no-new */
 new Vue({
